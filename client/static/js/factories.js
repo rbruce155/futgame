a24115d76e24b27bb6fffaaaf79c9059f9e9cda9
Robert Bruce <rbruce155@gmail.com>
2016-08-30 12:57:00 -0700

    updated login controller and factory

--- client/static/js/factories.js
+++ client/static/js/factories.js
@@ -1,21 +1,15 @@
 futgame_app.factory('loginFactory', function ($http) {
   var factory = {};
 
-  //Restful syntax: index = get all that object
-  factory.index = function(callback) {
-      $http.get('/names').success(function(output) {
+
+  factory.login = function (user, callback) {
+    $http.post('/login', user).success(function (output) {
       callback(output);
     })
   };
 
-  factory.create = function (newPerson, callback) {
-    $http.get('/new/' + newPerson.name + '/').success(function (output) {
-      callback(output);
-    })
-  };
-
-  factory.delete = function (person, callback) {
-      $http.get('/remove/'+ person.name +'/').success(function (output) {
+  factory.register = function (newUser, callback) {
+      $http.post('/signin', newUser).success(function (output) {
         callback(output);
       })
   };
