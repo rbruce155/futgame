4acc468a04893981e15ddae51b13e8afc814339d
Miguel Guerra <guetop88@gmail.com>
2016-08-29 17:18:15 -0700

    add login, signup backend, need to fix the session

---
+++ server/models/user.js
@@ -0,0 +1,50 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var bcrypt = require("bcryptjs");
+
+var UserSchema = new Schema({
+	username: {type: String, require: true},
+	password: {type: String, require: true},
+	email: {type: String, require: true, unique: true},
+	credit: {type: Number},
+	points: [{type: String}]
+});
+
+UserSchema.pre("save", function(next){
+	var user = this;
+	if(this.isModified("password") || this.isNew)
+	{
+		bcrypt.genSalt(10, function(err, salt){
+			if(err)
+			{
+				return next(err);
+			}
+			else
+			{
+				bcrypt.hash(user.password, salt, function(err, hash){
+					if(err)
+					{
+						return next(err);
+					}
+					else
+					{
+						user.password = hash;
+						next();
+					}
+				});
+			}
+		});
+	}
+	else
+	{
+		return next();
+	}
+});
+
+UserSchema.methods.comparePassword = function(passw){ 
+	return bcrypt.compareSync(passw, this.password)
+}
+
+mongoose.model("User", UserSchema);
+
+
